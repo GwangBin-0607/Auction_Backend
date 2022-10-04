6cc2e6ea32ca1921988b3397b9f28fec51ac9f09
GwangBin-0607 <rhkdqlsdml10@gmail.com>
2022-10-04 18:04:21 +0900

    Set

--- Backend/index.js
+++ Backend/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var app = express();
 const port = 3100;
 
-app.listen(port,function(){
-    console.log("Express server has started on port : "+port);
+app.listen(port, function () {
+    console.log(process.env.NODE_ENV)
+    console.log("Express server has started on port : " + port);
 });
 
-app.get('/',function(req,res){
-    res.send("Nginx startsssssswwwwwww`111111111aaa");
+app.get('/', function (req, res) {
+    var property = "Hello awdadada"
+    res.send(property);
 });
