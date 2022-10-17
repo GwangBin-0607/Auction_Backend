18371f22df9da1356b13c4f7861cde4b513f8f3c
GwangBin-0607 <rhkdqlsdml10@gmail.com>
2022-10-17 15:36:39 +0900

    Github Action

--- Backend/index.js
+++ Backend/index.js
@@ -6,8 +6,30 @@ app.listen(port, function () {
     console.log(process.env.NODE_ENV)
     console.log("Express server has started on port : " + port);
 });
+function tt(Path,Handler){
+    try{
+        Handler()
+    }catch(error){
+        console.log("ERROR")
+        console.log(error)
+    }
+}
 
-app.get('/', function (req, res) {
-    var property = "Hello awdadadaawadwadad"
+app.get('/home', function (req, res) {
+    var property = "Hello awdadadaawadwasdasdasdad"
+    tt("gg",()=>{
+        let t = new Test();
+        t.hello();
+    })
+    try{
+        let t = new Test();
+        t.hello();
+    }catch(error){
+        console.log(error);
+    }
+    console.log("hello");
     res.send(property);
 });
+class Test{
+
+}
