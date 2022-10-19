7f13fe0b47e9efeb36d1cad404f3414f07cf11a0
GwangBin-0607 <rhkdqlsdml10@gmail.com>
2022-10-19 23:51:52 +0900

    Sequelize Setting-Change package.json config.js

--- Backend/index.js
+++ Backend/index.js
@@ -2,19 +2,19 @@ var express = require('express');
 var app = express();
 require('dotenv').config();
 const port = process.env.PORT;
-const db=require('./Database/models');
-const User = db.db.users;
+// const db=require('./Database/models');
+// const User = db.db.users;
 
-const addUser = async (req, res) => {
-    let info = {
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email:req.body.email
-    };
+// const addUser = async (req, res) => {
+//     let info = {
+//         firstName: req.body.firstName,
+//         lastName: req.body.lastName,
+//         email:req.body.email
+//     };
   
-    const user = await User.create(info).catch((err) => console.log(err));
-    console.log(user);
-  };
+//     const user = await User.create(info).catch((err) => console.log(err));
+//     console.log(user);
+//   };
 app.listen(port, function () {
     console.log(process.env.NODE_ENV)
     console.log("Express server has started on port : " + port);
@@ -22,13 +22,13 @@ app.listen(port, function () {
 app.get('/home', async function (req, res) {
     var property = "CD Complete good"
 
-    const user = {
-        body:{
-        firstName:"Hello",
-        lastName:"1234",
-        email:"1231231a2"
-    }
-    };
-    await addUser(user,"hello");
+    // const user = {
+    //     body:{
+    //     firstName:"Hello",
+    //     lastName:"1234",
+    //     email:"1231231a2"
+    // }
+    // };
+    // await addUser(user,"hello");
     res.send(property);
 });
