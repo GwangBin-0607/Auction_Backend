dd976d49cb71a2af5dcfa6cd286298f2d2f03d6c
GwangBin-0607 <rhkdqlsdml10@gmail.com>
2022-10-19 18:03:45 +0900

    Sequelize Setting

--- Backend/Database/config/config.js
+++ Backend/Database/config/config.js
@@ -17,10 +17,11 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": env.PROD_USERNAME,
+    "password": env.PROD_USERNAME,
+    "database": env.PROD_USERNAME,
+    "host": env.PROD_USERNAME,
+    "host": env.PROD_USERNAME,
     "dialect": "mysql"
   }
 }
