2fd0bde7cd830ee2521073121b22cddbb7d3d13f
GwangBin-0607 <rhkdqlsdml10@gmail.com>
2022-10-24 18:49:46 +0900

    Docker,Docker-compose dev,prod

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ app.listen(port, function () {
     console.log("Express server has started on port : " + port);
 });
 app.get('/home', async function (req, res) {
-    var property = "CD Complete good Second!"
+    var property = "CD Complete good THIRD!"
 
     // const user = {
     //     body:{
