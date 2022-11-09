d4c2890e68b39aebd517b237115fc5d9e698d51d
GwangBin-0607 <rhkdqlsdml10@gmail.com>
2022-11-09 22:31:26 +0900

    First Commit

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var app = express();
+var app = require('express')();
+var server = require('http').createServer(app);
 require('dotenv').config();
 const port = process.env.PORT;
 // const db=require('./Database/models');
@@ -15,11 +15,12 @@ const port = process.env.PORT;
 //     const user = await User.create(info).catch((err) => console.log(err));
 //     console.log(user);
 //   };
-app.listen(port, function () {
+
+server.listen(port, function () {
     console.log(process.env.NODE_ENV)
     console.log("Express server has started on port : " + port);
 });
 app.get('/home', async function (req, res) {
-    var property = "CD Complete good THIRD!"
+    var property = "CD Complete good FOUR!"
     res.send(property);
 });
---
+++ Backend/indexTwo.js
@@ -0,0 +1,41 @@
+const net = require('net');
+const ipaddr = "localhost";
+const port = 3100;
+let clientArray=[];
+
+let server = net.createServer(function (socket) {
+	console.log(socket.address().port + " connected.");
+	socket.setEncoding('utf8');
+    clientArray.push(socket)
+    const json = {
+        id : 100,
+        name :"안광빈"
+    }
+    let jsonTwo = `{
+        "id" : "byeolgori502",
+        "name" : "별고리"
+    }`;
+	socket.on('data', function (data) {
+        console.log("Connect Data!!")
+		console.log(data);
+        // clientArray.forEach(client=>{
+        //     client.write("hello");
+        // });
+	});
+	socket.on('close', function () {
+		console.log('client disconnted.');
+	});
+	setTimeout(() => {
+		socket.write('welcome to server');
+	}, 500);
+});
+
+// print error message
+server.on('error', function (err) {
+	console.log('err: ', err.code);
+});
+
+// listening
+server.listen(port, ipaddr, function () {
+	console.log('listening on 2031..');
+});
