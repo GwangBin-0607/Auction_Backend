18c07ed5fa9c148aa17afb84a8e97d0eec695114
GwangBin-0607 <rhkdqlsdml10@gmail.com>
2022-12-07 23:42:05 +0900

    Create Product,Product_Images Table

---
+++ Backend/tcp/Database/migrations/20221207084628-create-product.js
@@ -0,0 +1,25 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Products', {
+      product_id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      product_name:{
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      product_price:{
+        allowNull: false,
+        type: Sequelize.INTEGER
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Products');
+  }
+};
---
+++ Backend/tcp/app.js
@@ -0,0 +1,77 @@
+const net = require('net');
+require('dotenv').config();
+const port = process.env.TCPPORT;
+let clientArray=[];
+
+const db=require('./Database/models');
+const User = db.product_images;
+
+const addUser = async () => {
+    let info = {
+        image_url:"222",
+        product_id:2
+    };
+  
+    const user = await User.create(info).catch((err) => console.log(err));
+    console.log(JSON.stringify(user));
+  };
+//   const list = async ()=>{
+//     const result = await db.products.findAll({
+//         include:
+//         {model:db.product_images}
+//         ,
+//         plain:true
+//     }).catch((err) => console.log(err));
+//     console.log(result);
+//     return result
+//   }
+const list = async ()=>{
+    const result = await db.products.findAll({
+        raw:true,
+        attributes:[['product_id','id'],['product_price','price']]
+    }).catch((err) => console.log(err));
+    console.log(result);
+    return result
+  }
+
+let server = net.createServer(function  (socket) {
+	console.log(socket.address() + " connected.");
+	socket.setEncoding('utf8');
+    clientArray.push(socket)
+    const json = [{
+        ida : 100,
+        price :123123
+    }]
+    let jsonTwo = `{
+        "id" : 1,
+        "price" : 1000
+    }`;
+    // socket.write("hello");
+	socket.on('data', function(data) {
+        console.log("Connect Data!!")
+		console.log(data);
+        console.log(JSON.parse(data).id);
+        clientArray.forEach(async client=>{
+            let result = await list();
+            console.log(result);
+            client.write(JSON.stringify(result));
+        });
+	});
+	socket.on('close', function () {
+		console.log('client disconnted.');
+	});
+	setTimeout(() => {
+		// socket.write('w111111elcome to server!!!!123!aasdasd!!!');
+	}, 500);
+});
+
+// print error message
+server.on('error', function (err) {
+    
+	console.log('err: ', err);
+});
+
+// listening
+server.listen(port, function () {
+	console.log('listening on'+port);
+});
