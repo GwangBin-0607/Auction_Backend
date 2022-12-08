a92f5bbb82ab43229700a2ea47f45d3a0b2e106f
GwangBin-0607 <rhkdqlsdml10@gmail.com>
2022-12-08 21:14:36 +0900

    Router Pattern && http delete sequelize-cli

--- Backend/http/app.js
+++ Backend/http/app.js
@@ -5,18 +5,6 @@ const port = process.env.PORT;
 const routes = require('./router');
 
 app.use('/aa', routes);
-const db=require('./Database/models/index');
-  const list = async ()=>{
-    const result = await db.products.findAll({
-        include:
-        {model:db.product_images}
-        ,
-        plain:true
-    }).catch((err) => console.log(err));
-    console.log(result);
-    return result
-  }
-  list();
 
 server.listen(port, function () {
     console.log(process.env.NODE_ENV)
--- Backend/http/router/productList/productList.controller.js
+++ Backend/http/router/productList/productList.controller.js
@@ -1,3 +1,15 @@
-exports.users = (req, res, next) => {
-    res.send('respond with a resource');
+const db=require('./Database/models/index');
+  const list = async ()=>{
+    const result = await db.products.findAll({
+        include:
+        {model:db.product_images}
+        ,
+        plain:true
+    }).catch((err) => console.log(err));
+    console.log(result);
+    return result
+  }
+exports.users = async (req, res, next) => {
+    var result = await list()
+    res.send(result);
 }
