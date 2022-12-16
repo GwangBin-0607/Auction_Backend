4dc441d9173d2e07091e169528b362ee03f7499e
GwangBin-0607 <rhkdqlsdml10@gmail.com>
2022-12-16 16:16:49 +0900

    Change Delegate

--- Backend/http/usecasea/product.price.usecase.js
+++
@@ -1,7 +0,0 @@
-const {product_prices} = require('../Database/models');
- async function allProductPriceList(){
-  const result = product_prices.findOne({
-  })
-  return result
-}
-exports.getList = allProductPriceList;
