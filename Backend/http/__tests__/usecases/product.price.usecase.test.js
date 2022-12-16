1a9178857a391674d9a86015494f930329ec5ddb
GwangBin-0607 <rhkdqlsdml10@gmail.com>
2022-12-16 16:07:58 +0900

    Change Delegate

--- Backend/http/__tests__/usecases/product.price.usecase.test.js
+++ Backend/http/__tests__/usecases/product.price.usecase.test.js
@@ -4,6 +4,8 @@ test('Product Price List', async () => {
     expect(usecase.getList()).toThrow()*/ //Error
 
   // await expect(usecase.getList()).toThrow(); //Error = ERR_UNHANDLED_REJECTION
-  const data = usecase.getList()
-  await expect(data).rejects.toThrow();
+  // const data = usecase.getList()
+  // await expect(data.count).resolves.toBe(5);
+  const data = await usecase.getList()
+  expect(data.length).toBe(5);
   });
--- Backend/http/usecase/product.price.usecase.js
+++ Backend/http/usecase/product.price.usecase.js
@@ -1,17 +1,6 @@
-const {product_images,product_prices} = require('../database/models');
+const {product_prices} = require('../database/models');
  async function allProductPriceList(){
   const result = product_prices.findAll({
-    include:[
-      {
-        model:product_images,
-        attributes:[['image_url','url']],
-        where:{
-          main_image:1
-        }
-      },{
-        model:product_prices
-      }
-    ]
   })
   return result
 }
